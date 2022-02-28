4c283eee9c301df955c38730b598d7a4d09c42c7
huyhoang <huyhoang1996ha@gmail.com>
2022-02-28 10:38:48 +0700

    Fix bubble

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+lst = [2,5,3,1,6,4]
+
+for index in range(len(lst)-1):
+	for index_next in range(index+1, len(lst)):
+		if lst[index] > lst[index_next]:
+			lst[index], lst[index_next] = lst[index_next], lst[index]
+
+
+print("=== lst ", lst)
+
